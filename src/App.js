bda0806603787b518d127b16d2fb8f9a00396921
SPoluliakh <S_Poluliakh@ukr.net>
2022-09-29 22:08:17 +0300

    added settings

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Profile from './components/Profile';
+import user from './user.json';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
+  );
+}
+
+export default App;
